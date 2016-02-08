8609c4e5322bb9ec38beaac9bf08d2e6ebc9b7af
Andrew Throener <athroener@gmail.com>
2016-02-08 16:46:09 -0600

    Coveralls and start gulp split

---
+++ tasks/gulp/coveralls.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp'),
+  concat = require('gulp-concat')
+  coveralls = require('gulp-coveralls');
+
+// Lint CSS and JavaScript files.
+gulp.task('coveralls', function (done) {
+  if (!process.env.CI) return done();
+  return gulp.src('.coverdata/*/lcov.info')
+    .pipe(concat('lcov.info'))
+    .pipe(coveralls());
+});
