25145f2915d825ccfb055b8c555b7595d5e4c2c2
James Ireland <james@siteshackinc.com>
2016-02-10 13:54:15 -0600

    fix(gulp) Build process changes

--- config/env/default.js
+++ config/env/default.js
@@ -29,11 +29,11 @@
     // for obsecurity reasons
     sessionKey: 'sessionId',
     sessionCollection: 'sessions',
-    logo: 'modules/core/client/img/brand/logo.png',
-    favicon: 'modules/core/client/img/brand/favicon.ico',
+    logo: './public/dist/img/core/client/img/brand/logo.png',
+    favicon: './public/dist/img/core/client/img/brand/favicon.ico',
     uploads: {
       profileUpload: {
-        dest: './modules/users/client/img/profile/uploads/', // Profile upload destination path
+        dest: './public/img/profile/uploads/', // Profile upload destination path
         limits: {
           fileSize: 1*1024*1024 // Max file size in bytes (1 MB)
         }
