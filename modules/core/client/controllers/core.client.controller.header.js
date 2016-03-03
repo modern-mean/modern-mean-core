9bdbfb3475d81532a0788b6cd04f99abceecb35b
James Ireland <james@siteshackinc.com>
2016-03-03 12:21:13 -0600

    feat(core) changed function

--- modules/core/client/controllers/core.client.controller.header.js
+++ modules/core/client/controllers/core.client.controller.header.js
@@ -5,39 +5,19 @@
     .module('core')
     .controller('HeaderToolbar', HeaderToolbar);
 
-  HeaderToolbar.$inject = ['$timeout', '$mdSidenav'];
+  HeaderToolbar.$inject = ['$timeout', '$mdSidenav', 'lodash'];
 
-  function HeaderToolbar($timeout, $mdSidenav) {
+  function HeaderToolbar($timeout, $mdSidenav, lodash) {
     var vm = this;
 
     vm.toggleLeft = buildDelayedToggler('left');
     vm.toggleRight = buildDelayedToggler('right');
 
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = vm,
-          args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
-
     function buildDelayedToggler(navID) {
-      return debounce(function() {
+      return lodash.debounce(function() {
         $mdSidenav(navID)
           .toggle()
       }, 200);
-    }
-
-    function buildToggler(navID) {
-      return function() {
-        $mdSidenav(navID)
-          .toggle()
-      }
     }
 
     console.log('HeaderToolbar::Init', vm);
